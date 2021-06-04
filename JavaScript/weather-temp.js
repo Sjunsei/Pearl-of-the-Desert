b151e49456f7c370025f9b633e1eed4c7b4942da
DavidJos-292710 <71811421+DavidJos-292710@users.noreply.github.com>
2021-06-04 11:05:43 +0400

    David June 4th 11:04am
    Adding a javascript for the weather in the homepage and need some help on adjusting the second temp box but for some reason the javascript isn't working for some reason when i tried searching it won't show

--- JavaScript/weather-temp.js
+++ JavaScript/weather-temp.js
@@ -1,25 +1,43 @@
-let maintemp, settemp = {
-    "apiKey" : "07e80a1d3422afdcd76bbd8ccb04fa58",
-    fetchweather: function (city) {
+let weather = {
+    "apiKey": "07e80a1d3422afdcd76bbd8ccb04fa58",
+    fetchWeather: function (city){
         fetch(
-            "http://api.openweathermap.org/data/2.5/weather?q=" 
-            + city 
-            + "&units=metirc&appid=" 
-            + appKey
-        )
-        .then((response) => response.json())
+        "http://api.openweathermap.org/data/2.5/weather?q=" 
+        + city
+        + "&units=metirc&appid=" 
+        + appKey
+        ).then((respone) => respone.json())
         .then((data) => this.displayWeather(data));
     },
-    displayWeather = function (data) {
+    displayWeather: function (data){
         const { name } = data;
-        const { icon, description } = data.maintemp,settemp;
+        const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log( name, icon, description, temp, humidity, speed)
-        document.querySelector(".temp1").innerText = temp + "°C";
-        document.querySelector(".subtemp").src =
-        "/Resources/Images/New Icons/Icon awesome-temperature-high.svg" + temp;
-        document.querySelector(".humidity").innerText = humidity + "%";
-        document.querySelector(".windSpeed").innerText = speed + "km/h";
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temperature").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".windSpeed").innerText = "Wind Speed: " + speed + "km/h";
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
+
+document
+    .querySelector(".search button")
+    .addEventListener("click" , function(){
+        weather.search();
+    });
+
+document
+    .querySelector(".search button")
+    .addEventListener("Enter" , function(event){
+        if(event.key == "Enter"){
+            weather.search();
+        }
+    });
