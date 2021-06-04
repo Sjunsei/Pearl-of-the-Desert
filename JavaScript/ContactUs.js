9e8356acc4e5edc447aa889a34fd4dab26b42a54
Sjunsei <71812546+Sjunsei@users.noreply.github.com>
2021-06-04 22:11:35 +0400

    Minor Additions

--- JavaScript/ContactUs.js
+++ JavaScript/ContactUs.js
@@ -2,7 +2,41 @@ console.log('Start')
 
 $(document).ready(function () {
     $('.submit').click(function(event) {
-        event.preventDefault();
         console.log('send id clicked')
+
+        var email = $('.E-mail').val()
+        var firstname = $('.First_Name').val()
+        var lastname = $('.Last_Name').val()
+        var message = $('.msg').val()
+        var statusE = $('.status')
+        statusE.empty()
+
+        if( firstname.length >= 2) {
+            statusE.append('<div>First name is valid</div>')
+        } else {
+            statusE.append('<div>First name is invalid</div>')
+            event.preventDefault();
+        }
+
+        if( lastname.length >= 2) {
+            statusE.append('<div>Last is valid</div>')
+        } else {
+            statusE.append('<div>Last name is invalid</div>')
+            event.preventDefault();
+        }
+
+        if(email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusE.append('<div>Email is valid</div>')
+        } else {
+            statusE.append('<div>Email is invalid</div>')
+            event.preventDefault();
+        }
+        
+        if( message.length > 20) {
+            statusE.append('<div>message is valid</div>')
+        } else {
+            statusE.append('<div>message is invalid</div>')
+            event.preventDefault();
+        }
     })
 })
